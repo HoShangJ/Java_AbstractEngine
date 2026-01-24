eac28fe0b9c67afc5517f4fdd527f1ecf23586a1
HoShangJ <hoshangjay@hotmail.com>
2026-01-24 14:42:46 +0800

    Added AbstractEntity and EntityManager Classes

---
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/EntityManager.java
@@ -0,0 +1,7 @@
+package io.github.some_example_name.lwjgl3;
+
+import java.util.ArrayList;
+
+public class EntityManager {
+    private List<AbstractEntity> entities = new ArrayList<>();
+}
---
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
@@ -0,0 +1,6 @@
+package io.github.some_example_name.lwjgl3;
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class GameMaster extends ApplicationAdapter {
+
+}
